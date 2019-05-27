4896774bedc4aabe59ac8f414f323d7104b733df
fywest <fywest2109@hotmail.com>
2019-05-27 13:54:01 +0300

    test.html add

---
+++ test.js
@@ -0,0 +1,15 @@
+console.log("hello world")
+var number=5;// in-line comment
+/*
+data type
+*/
+var myName = "Beau";
+myName = 8;
+let ourName = "freeodeCamp";
+const pi = 3.14;
+var a;
+var b = 2;
+a = 7;
+b = a;
+console.log(a);
+alert("no hello")
