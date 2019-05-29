c87b4b9b32a17fba0ae69e60994c435de6785e94
fywest <fywest2109@hotmail.com>
2019-05-29 23:53:03 +0300

    update 30 min

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 console.log("hello world")
-var number=5;// in-line comment
+var number = 5;// in-line comment
 /*
 data type
 */
@@ -12,4 +12,30 @@ var b = 2;
 a = 7;
 b = a;
 console.log(a);
+a += 12;
+console.log(a);
 //alert("no hello")
+var sum = 10 + 10;//+-*\
+console.log(sum);
+sum++;
+console.log(sum);
+var remainder;
+remainder = 11 % 3;
+console.log(remainder);
+var myName = "Jennifer";
+console.log(myName);
+var myStr = "I am a \"double quoted\" string inside \"double quotes\"";
+console.log(myStr);
+var myLink = '<a href="http://www.example.com" target="_blank">Link</a>'
+console.log(myLink);
+/***
+ code output \' single quote \" double quote \\backslash
+ \n new line  \r tab \b backspace \f form feed
+ */
+myStr = "FirstLine\n\t\\SecondLine\nThirdLine";
+console.log(myStr);
+myStr = "This is the start. " + "This is the end. ";
+myStr += "This is the third. ";
+myStr += "\n"+myLink;
+console.log(myStr);
+//************30 minutes */
