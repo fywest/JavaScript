499a6e519d64527f4c9455b758f19f13e974c82e
fywest <fywest2109@hotmail.com>
2019-05-31 23:52:24 +0300

    update lookupprofile function

--- test.js
+++ test.js
@@ -454,3 +454,57 @@ var product=multiplyAll([[1,2],[3,4],[5,6,7]])
 console.log(product)
 
 // 2:24 min profile lookup
+
+var contacts = [
+    {
+        "firstName":"Lorna",
+        "lastName":"Cruickshank",
+        "number":"5421590221",
+        "likes":["Apple","Oringe","Banana"]
+    },
+    {
+        "firstName":"Akira",
+        "lastName":"Laine",
+        "number":"0543236543",
+        "likes":["Hogwarts","Magic","Hagrid"]
+    },
+    {
+        "firstName":"Harry",
+        "lastName":"Potter",
+        "number":"0994372684",
+        "likes":["Pizza","Coding","Brownie Points"]
+    },
+    {
+        "firstName":"Sherlock",
+        "lastName":"Holmes",
+        "number":"0487345643",
+        "likes":["Intriguing Cases","Violin"]
+    }
+]
+
+function lookUpProfile(name, Prop){
+    //console.log(name,Prop)
+    //console.log(contacts.length)
+    for (var i=0;i<contacts.length;i++){
+        //console.log(contacts[i].firstName,name);
+        if(contacts[i].firstName === name){
+            //console.log(contacts[i][Prop]);
+            return contacts[i][Prop]||"No Such Property"
+            // if(contacts[i].hasOwnProperty(Prop)){
+            //     return contacts[i][Prop];
+            // }else{
+            //     return "Not Such Property";
+            // }
+        }
+    }
+    return "No Such Name"
+}
+
+var data1 = lookUpProfile("Harry","likes");
+console.log(data1);
+data1 = lookUpProfile("Sherlock","number");
+console.log(data1);
+data1 = lookUpProfile("Sherlock1","number");
+console.log(data1);
+data1 = lookUpProfile("Sherlock","number1");
+console.log(data1);
