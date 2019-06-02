9ac3380d6e163937060daa570573d8f3ff8260dd
fywest <fywest2109@hotmail.com>
2019-06-02 23:31:41 +0300

    tony weird parts added

---
+++ test_tony.js
@@ -0,0 +1,27 @@
+var a = 'Hello World!'
+
+function b(){
+    console.log('Called b!');
+}
+
+b();
+console.log(a);
+// 38 min
+
+/*  
+var Address = {
+    Street: 'Main',
+    Number: 100
+}
+var Apartment={
+    Floor: 3,
+    Number:301
+}
+
+var Address_object={
+    Address,
+    Apartment
+};
+
+console.log(Address_object);
+*/
