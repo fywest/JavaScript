3d1a92ef97ab84164ecc169a921cc610b3b88642
fywest <fywest2109@hotmail.com>
2019-06-07 00:32:31 +0300

    update

--- test_tony.js
+++ test_tony.js
@@ -1,3 +1,44 @@
+
+console.log(libraryName);
+/* 
+function greet(name){
+    name = name || '<your name here>';
+    console.log('Hello '+ name);
+}
+greet('Tony');
+greet();
+*/
+
+
+/*
+var a = 0;
+var b = 1//0//false;
+
+if(a == b){
+    console.log('They are equal at === ')
+    if(a === b){
+        console.log('also they are equal at === ')//!==
+    }else{
+        console.log(' but they not equal at === ')
+    }
+}
+else{
+    console.log('the are not equal !')
+}
+console.log(Boolean(undefined));
+console.log(Boolean(null));
+console.log(Boolean(''));
+console.log(Boolean(0));
+
+var c='he';
+if(c){
+    console.log('There is something');//c||c===0
+} 
+
+*/
+
+
+/* 
 var a = 3 + 4 ;
 console.log(a);
 var b = 3 > 4;
@@ -9,6 +50,9 @@ console.log(c);
 console.log(1 < 2 < 3)
 console.log(3 < 2 < 1)
 console.log(Number(false))
+*/
+
+
 /*primitive
 undefined, null, boolean, number, string, symbol
 */
